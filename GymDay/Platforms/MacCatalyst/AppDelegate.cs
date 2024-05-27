c345fbbaf4755b9059913aa639ef7d5ae53220e5
Mikael Bernau <mikael.bernau@gmail.com>
2024-05-27 14:56:26 +0200

    init

---
+++ GymDay/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GymDay
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
